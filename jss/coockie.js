8f7eae27df47b86f455570db168643e3bc730e9f
UnaiGallardon <ugallarfer@educacion.navarra.es>
2024-02-27 11:41:02 +0100

    .

--- jss/coockie.js
+++ jss/coockie.js
@@ -1,28 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var cookieBanner = document.createElement('div');
-    cookieBanner.className = 'cookie-banner';
-    cookieBanner.innerHTML = `
-        <p>Este sitio web utiliza cookies. Al continuar utilizando este sitio, aceptas nuestro uso de cookies.</p>
-        <button id="accept-btn">Aceptar</button>
-        <button id="reject-btn">Rechazar</button>
-    `;
-    
-    document.body.appendChild(cookieBanner);
-
+    var cookieBanner = document.getElementById('cookie-banner');
     var acceptBtn = document.getElementById('accept-btn');
     var rejectBtn = document.getElementById('reject-btn');
 
+    // Verificar si la cookie de aceptación está presente
     var cookieAccepted = localStorage.getItem('cookieAccepted');
 
     if (!cookieAccepted) {
         cookieBanner.style.display = 'block';
     }
 
+    // Manejar clic en el botón de aceptar
     acceptBtn.addEventListener('click', function () {
         localStorage.setItem('cookieAccepted', 'true');
         cookieBanner.style.display = 'none';
     });
 
+    // Manejar clic en el botón de rechazar
     rejectBtn.addEventListener('click', function () {
         // Puedes manejar el rechazo según tus necesidades, por ejemplo, redirigir o cerrar la página
         cookieBanner.style.display = 'none';
